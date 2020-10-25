4c0ab53bed8d22331d2674d781d1a56c77f37e35
Fernando Galicia <fernandojo.galicia@gmail.com>
2020-10-24 23:08:35 -0600

    Publicación de login y estado de publicación

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
 from .models import Publicacion
 from .forms import PublicacionForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def publicacion_lista (request):
@@ -12,19 +13,20 @@ def publicacion_detalle(request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
 
+@login_required
 def publicacion_nueva(request):
     if request.method == "POST":
         formulario = PublicacionForm(request.POST)
         if formulario.is_valid():
             publicacion = formulario.save(commit=False)
             publicacion.autor = request.user
-            publicacion.fecha_publicacion = timezone.now()
             publicacion.save()
             return redirect('detalle_publicacion', pk=publicacion.pk)
     else:
         formulario = PublicacionForm()
     return render(request, 'blog/publicacion_editar.html', {'formulario':formulario})
 
+@login_required
 def publicacion_editar(request, pk):
     publicacion = get_object_or_404(Publicacion, pk=pk)
     if request.method == "POST":
@@ -32,9 +34,25 @@ def publicacion_editar(request, pk):
         if formulario.is_valid():
             publicacion = formulario.save(commit=False)
             publicacion.autor = request.user
-            publicacion.fecha_publicacion = timezone.now()
             publicacion.save()
             return redirect('detalle_publicacion', pk=publicacion.pk)
     else:
         formulario = PublicacionForm(instance=publicacion)
     return render(request, 'blog/publicacion_editar.html', {'formulario': formulario})
+
+@login_required
+def publicacion_borrador_lista(request):
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
+    return render(request, 'blog/publicacion_borrador_lista.html', {'publicaciones': publicaciones})
+
+@login_required
+def publicacion_publicar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    publicacion.publicar()
+    return redirect('detalle_publicacion', pk=pk)
+
+@login_required
+def publicacion_borrar(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    publicacion.delete()
+    return redirect('publicacion_lista')
